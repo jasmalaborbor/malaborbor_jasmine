36711077f3d57edde221cb3138adb6e289672575
jasmalaborbor <jasminemalaborbor0912@gmail.com>
2025-09-25 12:40:37 +0800

    commit

--- app/controllers/CreateAuthor.php
+++ app/controllers/CreateAuthor.php
@@ -1,8 +1,17 @@
 <?php
 class CreateAuthor extends Controller {
     public function index() {
-        // Logic for creating an author
-        // ...
+        if ($this->io->method() === 'post') {
+            $name = $this->io->post('name');
+            if (!empty($name)) {
+                $this->db->table('authors')->insert(['first_name' => $name]);
+                redirect('/author');
+            } else {
+                $data['error'] = 'Name is required.';
+                $this->view('create_author', $data);
+                return;
+            }
+        }
         $this->view('create_author');
     }
 }
--- app/controllers/DeleteAuthor.php
+++ app/controllers/DeleteAuthor.php
@@ -1,8 +1,22 @@
 <?php
 class DeleteAuthor extends Controller {
-    public function index($id) {
-        // Logic for deleting an author
-        // ...
-        $this->view('delete_author', ['id' => $id]);
+    public function index($id = null) {
+        if ($this->io->method() === 'post') {
+            $id = $this->io->post('id');
+            if (!empty($id)) {
+                $this->db->table('authors')->where('id', $id)->delete();
+                redirect('/author');
+            } else {
+                $data['error'] = 'ID is required.';
+                $data['id'] = $id;
+                $this->view('delete_author', $data);
+                return;
+            }
+        } else if (!empty($id)) {
+            $data['id'] = $id;
+            $this->view('delete_author', $data);
+        } else {
+            redirect('/author');
+        }
     }
 }
